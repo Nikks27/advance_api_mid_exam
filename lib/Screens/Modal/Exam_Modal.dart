4c8df05e43008e8ad5ec0a0f6d7fad2fe1fb78f9
RABARI NARESH <148762716+Nikks27@users.noreply.github.com>
2024-08-07 16:47:47 +0530

    Update Exam_Modal.dart

--- lib/Screens/Modal/Exam_Modal.dart
+++ lib/Screens/Modal/Exam_Modal.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 
-class JokesModal
+class Randomjokes
 {
   late String type,setup,punchline;
   late int id;
 
-  JokesModal(this.type, this.setup, this.punchline, this.id);
+  Randomjokes(this.type, this.setup, this.punchline, this.id);
 
-  factory JokesModal.fromJson(Map m1)
+  factory Randomjokes.fromJson(Map m1)
   {
-    return JokesModal(m1['type'] ?? '', m1['setup'] ?? '', m1['punchline'] ?? '', m1['id'] ?? 0);
+    return Randomjokes(m1['type'] ?? '', m1['setup'] ?? '', m1['punchline'] ?? '', m1['id'] ?? 0);
   }
 }
